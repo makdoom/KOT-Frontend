c95744305f9e275bf90312441aeb4d57e80fe347
Makdoom Shaikh <makshaikh99@gmail.com>
2024-05-20 19:41:10 +0530

    feat: Add Login page

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,6 +1,55 @@
+import BackgroundImage from "@/assets/background.jpg";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
-export default Login
+    <div className="w-screen h-screen flex gap-2 bg-[#f8fafc]">
+      <div className="flex-1 md:flex-[0.6] flex justify-center items-center">
+        <div className="w-full max-w-sm m-4 bg-white p-10 shadow-sm">
+          <h1 className="font-extrabold text-4xl tracking-wider text-center">
+            KOT
+          </h1>
+
+          <div className="mt-7 flex flex-col">
+            <div className="w-full max-w-sm mb-6">
+              <Label htmlFor="email">Username</Label>
+              <Input autoFocus type="email" id="email" placeholder="Username" />
+            </div>
+            <div className="w-full max-w-sm">
+              <Label htmlFor="password">Password</Label>
+              <Input type="password" id="password" placeholder="Password" />
+            </div>
+            <div className="text-right mt-2">
+              <Link to="/singup" className="text-blue-500 text-sm text-right">
+                Forgot Password ?
+              </Link>
+            </div>
+
+            <Button className="mt-6">Login</Button>
+            <div className="mt-3">
+              <p to="/singup" className="text-sm">
+                Already have an account ?{" "}
+                <Link to="/signup" className="text-blue-500">
+                  Signup
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex-[0.4] hidden md:block">
+        <div className="rounded-tl-3xl rounded-bl-3xl h-full">
+          <img
+            src={BackgroundImage}
+            alt="background"
+            className="h-full w-full object-cover rounded-tl-[70px] rounded-bl-[70px]"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Login;
