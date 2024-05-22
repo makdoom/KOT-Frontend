602ceee1df2c85a529f4da916e1994c7a8fdbc73
Makdoom Shaikh <makshaikh99@gmail.com>
2024-05-22 21:17:40 +0530

    feat: Add validation with hook-form and zod

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -2,9 +2,30 @@ import BackgroundImage from "@/assets/background.jpg";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { LoginSchema } from "@/schemas/authSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
+type LoginType = {
+  username: string;
+  password: string;
+};
+
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty, isValid, isSubmitting },
+  } = useForm<LoginType>({
+    mode: "onBlur",
+    resolver: zodResolver(LoginSchema),
+  });
+
+  const onSubmit = (data: LoginType) => {
+    console.log(data);
+  };
+
   return (
     <div className="w-screen h-screen flex gap-2 bg-[#f8fafc]">
       <div className="flex-1 md:flex-[0.6] flex justify-center items-center">
@@ -13,39 +34,57 @@ const Login = () => {
             KOT
           </h1>
 
-          <div className="mt-7 flex flex-col">
-            <div className="w-full  max-w-sm  mb-6">
-              <Label htmlFor="email">Username</Label>
-              <Input
-                autoFocus
-                type="email"
-                id="email"
-                placeholder="Username"
-                className="h-11"
-              />
-            </div>
-            <div className="w-full  max-w-sm ">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                type="password"
-                id="password"
-                placeholder="Password"
-                className="h-11"
-              />
-            </div>
-            <div className="text-right mt-2">
-              <Link
-                to="/forgot-password"
-                tabIndex={-1}
-                className="text-blue-500 text-sm text-right"
-              >
-                Forgot Password ?
-              </Link>
-            </div>
+          <div className="mt-7 ">
+            <form
+              noValidate
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex flex-col"
+            >
+              <div className="w-full  max-w-sm  mb-6">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  autoFocus
+                  type="username"
+                  id="username"
+                  placeholder="Username"
+                  className="h-11"
+                  {...register("username")}
+                />
+                <p className="mt-1 text-sm text-red-500">
+                  {errors.username?.message}
+                </p>
+              </div>
+              <div className="w-full  max-w-sm ">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  placeholder="Password"
+                  className="h-11"
+                  {...register("password")}
+                />
+                <p className="mt-1 text-sm text-red-500">
+                  {errors.password?.message}
+                </p>
+              </div>
+              <div className="text-right mt-2">
+                <Link
+                  to="/forgot-password"
+                  tabIndex={-1}
+                  className="text-blue-500 text-sm text-right"
+                >
+                  Forgot Password ?
+                </Link>
+              </div>
 
-            <Button className="mt-6" size="lg">
-              Login
-            </Button>
+              <Button
+                disabled={!isDirty || !isValid || isSubmitting}
+                className="mt-6"
+                size="lg"
+              >
+                Login
+              </Button>
+            </form>
             <div className="mt-3">
               <p className="text-sm text-muted-foreground">
                 Don't have an account ?{" "}
---
+++ src/schemas/authSchema.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export const SignupSchema = z
+  .object({
+    username: z.string().min(3, "Username is required"),
+    email: z
+      .string()
+      .min(1, "Email is required")
+      .email("Please enter a valid email"),
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .max(50, "Password must be at most 50 characters long"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
+
+export const LoginSchema = z.object({
+  username: z.string().min(3, "Username is required"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .max(50, "Password must be at most 50 characters long"),
+});
